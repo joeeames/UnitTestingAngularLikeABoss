9f8aaf4114bc15b3fd722dba834f5926748a9e85
Joe Eames <joeeames@gmail.com>
2018-04-09 18:45:50 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Unit Testing Angular Like a Boss
+
+To get started, clone the repo or download it
+
+npm install
+npm test
---
+++ src/app/strength/strength.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'strength'
+})
+export class StrengthPipe implements PipeTransform {
+  transform(value: number): string {
+    if(value < 10) {
+      return value + " (weak)";
+    } else if(value >= 10 && value < 20) {
+      return value + " (strong)";
+    } else {
+      return value + " (unbelievable)";
+    }
+  }
+}
